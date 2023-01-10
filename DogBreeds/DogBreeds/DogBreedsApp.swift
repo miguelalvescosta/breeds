13f6525c71b709da4a158a7f547ad16fcbfffcc1
miguelcbring <78351410+miguelcbring@users.noreply.github.com>
2023-01-10 11:47:06 +0000

    initial commit

---
+++ DogBreeds/DogBreeds/DogBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogBreedsApp.swift
+//  DogBreeds
+//
+//  Created by Miguel Costa on 10.01.23.
+//
+
+import SwiftUI
+
+@main
+struct DogBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
